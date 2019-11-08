0f799a0255f80c98b47d93c3dd57336d3c9ad7d0
Aleemendoza <alecolomendoza@gmail.com>
2019-11-08 13:49:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const cuadrados = document.getElementsByClassName("square");
+
+var pickedColor = document.getElementById("colorDisplay");
+
+var colors = ["rgb(240, 14, 128)","rgb(180, 76, 122)","rgb(255, 89, 00)",
+"rgb(23, 43, 34)", "rgb(34, 54, 98)", "rgb(43, 156, 128)"]
+
+pickedColor.textContent= pickedColor
+pickedColor.innerHTML=colors[4]
+
+
+
+// funcion para comparar el color clickeado con el del elemento del dom.
+function elegirCuadrados() {
+    for (i = 0; i<colors.length;i++){
+        
+        
+        cuadrados[i].addEventListener("click",function(){
+            var colorClickeado = this.style.backgroundColor;
+            
+           if (colorClickeado != pickedColor.innerHTML){
+            
+            this.style.background="gray"
+            pickedColor
+
+           }
+            else {  
+                var message = document.getElementById("message");
+                var h1 = document.querySelector("h1");
+                console.log(h1)
+                message.innerHTML="Correct!";
+                h1.style.backgroundColor=colorClickeado;
+            }
+        })   
+            cuadrados[i].style.backgroundColor=colors[i];
+    }  
+    return;
+} 
+elegirCuadrados();
+
+// funcion para que se vayan todos a la mierda
+
+
+
